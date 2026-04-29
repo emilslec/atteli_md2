06ba2859e504e3769ad65a9666815d487033cc7c
emilsles <emils.lecinskis@gmail.com>
2026-04-29 18:09:05 +0300

    Faili

--- src/main.js
+++
@@ -1,5 +0,0 @@
-var canvas = document.getElementById("particleCanvas");
-var ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-console.log(canvas.height);
